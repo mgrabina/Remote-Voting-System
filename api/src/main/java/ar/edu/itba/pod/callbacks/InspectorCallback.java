61674dc0af2de7bc28b80ad51f8e91cf98e73c02
Martin <martinnagelberg@gmail.com>
2019-09-15 16:02:10 -0300

    inspector service fixed

--- api/src/main/java/ar/edu/itba/pod/callbacks/InspectorCallback.java
+++ api/src/main/java/ar/edu/itba/pod/callbacks/InspectorCallback.java
@@ -1,7 +1,8 @@
 package ar.edu.itba.pod.callbacks;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface InspectorCallback {
+public interface InspectorCallback extends Remote {
     void inspect() throws RemoteException;
 }
